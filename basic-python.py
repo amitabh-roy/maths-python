089685c59494756bf4a512a493acb78b3255f68b
Amit <amitabh@codelogicx.com>
2025-07-30 19:36:17 +0530

    set up in other laptop

--- basic-python.py
+++ basic-python.py
@@ -17,11 +17,3 @@ print(stats.variance(numbers))  # Variance of the numbers
 print(stats.pstdev(numbers))  # Population standard deviation
 
 
-rollings = np.random.randint(1, 7, 1000)
-
-fig, ax = plt.subplots(figsize=(9, 9))
-n, bins, patches = ax.hist(rollings, bins=6)
-ax.set_title("Frequency Histogram of 1000 Times of Rolling a Dice", size=19)
-ax.set_xlim(0, 7)
-ax.set_ylim(0, 400)
-plt.show()
