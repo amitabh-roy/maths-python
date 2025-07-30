d5ac0efb6dba8619f5383886536f03edfb210da5
Amit <amitabh@codelogicx.com>
2025-07-30 19:38:03 +0530

    fixes

--- descriptive-stats.py
+++ descriptive-stats.py
@@ -1,4 +1,5 @@
 """
+https://ethanweed.github.io/pythonbook/landingpage.html
 https://github.com/weijie-chen/Basic-Statistics-With-Python
 https://realpython.com/python-statistics/
 """
---
+++ linear-algebra.py
@@ -0,0 +1,6 @@
+
+"""
+https://github.com/weijie-chen/Linear-Algebra-With-Python/tree/master/notebooks
+https://realpython.com/python-linear-algebra/
+https://pabloinsente.github.io/intro-linear-algebra
+"""
